83f2287f75036a3d23f65001a33619506fac9982
jeck54798 <131548237+jeck54798@users.noreply.github.com>
2024-08-23 11:26:23 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,33 @@
+teste = "Olá"
+print(type(teste))
+teste = 30
+print(type(teste))
+teste = 2.4
+print(type(teste))
+
+nome = input("Digite seu nome: ")
+print("Óla ", nome)
+idade  = input("Digite a sua idade: ")
+print(idade)
+print(type(idade))
+
+#Tipos de Dados:
+#int: números inteiros
+#float: números reais (decimais)
+#str: (string) cadeia de caracteres, utilizando aspas
+#boolean: tipo lógico true ou false
+
+valorA = int(input("Digite valor A: "))
+valorB = int(input("Digite valor B: "))
+print(valorA + valorB)
+print(type(valorA))
+
+# Operadores Aritméticos:
+x = 3
+print(x + 1)    # Adição
+print(x - 1)    # Subtração
+print(x * 2)    # Multiplicação
+print(x / 2)    # Divisão
+print(x // 2)   # Divisão interia
+print(x ** 2)   # Exponenciação
+print(x % 2)    # Resto da divisão
