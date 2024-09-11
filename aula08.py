e5b859d6d268ad5f6fe2eb0ef74c2de1c5ad4f0f
jeck54798 <131548237+jeck54798@users.noreply.github.com>
2024-09-11 11:58:13 -0300

    Add files via upload

---
+++ aula08.py
@@ -0,0 +1,10 @@
+#função
+def soma(a,b):
+    return a + b
+
+
+valorA = int(input("Digite um valor: "))
+valorB = int(input("Digite um valor: "))
+
+resultado = soma(valorA,valorB)
+print(f"A soma dos vlores é igual a {resultado}")
